ea0625b719b37dc02ad34a623d72f6a4b3c669b0
fernandezalexia57-has <fernandezalexia57@gmail.com>
2026-06-22 10:36:09 -0600

    Mejorando la consulta de libros

--- dao/libro_dao.py
+++ dao/libro_dao.py
@@ -12,7 +12,7 @@ class LibroDAO:
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
 
-        cursor.execute("SELECT * FROM libro")
+        cursor.execute("SELECT * FROM vista_libros" )
         registros = cursor.fetchall()
 
         libros = []
@@ -32,8 +32,8 @@ class LibroDAO:
         conexion = Conexion.obtener_conexion()
         cursor = conexion.cursor()
         sql="""
-        INSERT INTO libro (titulo, autor, isbn, disponible)
-        VALUES (%s, %s, %s, %s)
+        INSERT INTO libro (id, titulo, autor, isbn, disponible)
+        VALUES (%s, %s, %s, %s, %s)
         """
 
         cursor.execute(sql,
@@ -80,3 +80,16 @@ class LibroDAO:
         cursor.close()
         conexion.close()
 
+    def obtener_ultimo_id(self):
+        conexion = Conexion.obtener_conexion()
+        cursor = conexion.cursor()
+
+        cursor.execute("SELECT id FROM libro ORDER BY id DESC")
+        resultado = cursor.fetchone()
+
+        cursor.close()
+        conexion.close()
+
+        if resultado is None:
+            return 0
+        return resultado[0]
